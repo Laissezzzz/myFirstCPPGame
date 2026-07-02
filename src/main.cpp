2f56409638344577f08a81c31f7aa9be09105c72
Tommaso Pizzini <tom.97@alice.it>
2026-07-02 11:04:08 +0200

    raylib test working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+#include "raylib.h"
 
 int main()
 {
-	std::cout << "Hello stranger\n";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-	return 0;
+    InitWindow(screenWidth, screenHeight, "Raylib CMake Test");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Raylib works!", 280, 200, 30, DARKGREEN);
+        DrawCircle(400, 280, 50, SKYBLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
